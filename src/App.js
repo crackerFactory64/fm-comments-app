631dc26f8cb7ed7fe142303dae6bc5e1403e510e
crackerFactory <lee.mander@gmail.com>
2022-05-17 14:36:13 +0100

    setup and Comment component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./style.css";
+import Comment from "./Comment";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="comments">
+        <Comment
+          user="Lee"
+          img="./images/avatars/image-maxblagun.png"
+          loggedIn={false}
+          age="1 week ago"
+          body="Hello! How are you? Me, I am fine thank you very much."
+          score="1"
+        />
+      </section>
     </div>
   );
 }
-
-export default App;
---
+++ src/Comment.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Comment(props) {
+  return (
+    <article className="comment">
+      <div className="row comment__header">
+        <img className="comment__img" src={props.img} alt={props.user} />
+        <p className="comment__user">{props.user}</p>
+        <p className={props.loggedIn ? "comment__you" : "hide"}>you</p>
+        <p className="comment__age">{props.age}</p>
+      </div>
+      <p className="comment__body">{props.body}</p>
+      <div className="row comment__footer">
+        <div className="row comment__score">
+          <button
+            className="comment__vote-btn up"
+            aria-label="vote up"
+            id="upvote"
+          ></button>
+          <span>{props.score}</span>
+          <button
+            className="comment__vote-btn down"
+            aria-label="vote down"
+            id="downvote"
+          ></button>
+        </div>
+        <button className="comment__reply-btn">Reply</button>
+      </div>
+    </article>
+  );
+}
